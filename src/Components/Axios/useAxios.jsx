7d5edcbc5c5eecfaffe82df7735344ff12dad04e
unknown <mahmudhasan.hb@gmail.com>
2024-04-30 21:49:54 +0600

    working on order section

--- src/Components/Axios/useAxios.jsx
+++ src/Components/Axios/useAxios.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const AxiosSource = axios.create({
-    // baseURL: "https://zingzest-server.vercel.app",
-    baseURL: "http://localhost:2000",
-    withCredentials : true
+    baseURL: "https://zingzest-server.vercel.app",
+    // baseURL: "http://localhost:2000",
+    withCredentials: true
 })
 const useAxios = () => {
     return AxiosSource
--- src/Components/User/MyOrders.jsx
+++ src/Components/User/MyOrders.jsx
@@ -14,7 +14,7 @@ const MyOrders = () => {
     return (
         <section>
             <h1 className="text-4xl text-center font-bold my-10">My Orders</h1>
-            <div className="lg:w-3/5  mx-auto">
+            <div className="lg:w-3/5 space-y-3 my-10 mx-auto">
                 {
                     data == "l" ?
                         <MyOrderLoader></MyOrderLoader>
@@ -34,21 +34,21 @@ const MyOrders = () => {
 
 const Cards = ({ card }) => {
     return (
-        <div className="flex w-full my-2 bg-slate-100 text-black text-lg font-semibold rounded-xl p-2 justify-between border-2">
-            <div className="flex gap-10">
-                <img className="w-16 object-cover" src={card.image} alt="" />
-                <div className="my-auto">
-                    <h1>{card.name}</h1>
-                    <h1>{card.brand}</h1>
-                </div>
-                <div className="my-auto">
-                    <h1>Price: {card.price}</h1>
-                    <h1>Quantity: {card.quantity}</h1>
-                    <h1>Size: {card.size}</h1>
-
-                </div>
+        <div className="flex w-full bg-slate-100 text-black text-lg font-semibold rounded-xl p-2 justify-around border-2">
+            <div className="w-full ">
+                <img className="w-28 mx-auto h-full object-cover" src={card.image} alt="" />
             </div>
-            <div className="my-auto">
+            <div className="my-auto w-full">
+                <h1>{card.name}</h1>
+                <h1>{card.brand}</h1>
+            </div>
+            <div className="my-auto w-full">
+                <h1>Price: {card.price}</h1>
+                <h1>Quantity: {card.quantity}</h1>
+                <h1>Size: {card.size}</h1>
+
+            </div>
+            <div className="my-auto w-full">
                 <h1><span>Status: </span><span className="border-2 bg-white p-1 rounded-xl">pending</span></h1>
             </div>
         </div>
